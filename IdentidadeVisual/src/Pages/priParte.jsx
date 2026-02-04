8a1465eb8f02ec1009978caa2fcd0c4236e8896f
leoporto166 <leonardo.porto166@gmail.com>
2026-02-04 19:59:09 -0300

    SwiperSlide

--- IdentidadeVisual/src/Pages/priParte.jsx
+++ IdentidadeVisual/src/Pages/priParte.jsx
@@ -6,33 +6,41 @@ import case2 from "../img/projeto2.png"
 import case2Dois from "../img/projeto2.2.png"
 import case2Tres from "../img/projeto2.3.png"
 
+
+import { Swiper, SwiperSlide } from "swiper/react"
+import { Autoplay, Navigation, Pagination } from "swiper/modules"
+
+import "swiper/css"
+import "swiper/css/pagination"
+
 export default function PriParte(){
 
 return(
 
-    <body className="p-5 flex flex-col justify-center items-center">
+    <body className="flex flex-col justify-center items-center">
 
-        <div className="w-85 mt-10 ">
-            <div className="tudo">
-                <section className="hero">
-  <div className="sliders-bg gap-2">
-    <div className="slider-row">
-      <div className="slider gap-2">
-        <img src={case1} />
-        <img src={case1Dois} />
-        <img src={case1Tres} />
-        <img src={case1} />
-      </div>
-    </div>
+    <div className=" mt-10 p-5 flex flex-col justify-center
+    items-center ">
+        <div className="tudo">
+            <section className="hero">
+                <div className="sliders-bg gap-2">
+                    <div className="slider-row">
+                        <div className="slider gap-2">
+                            <img src={case1} />
+                            <img src={case1Dois} />
+                            <img src={case1Tres} />
+                            <img src={case1} />
+                        </div>
+                    </div>
 
-    <div className="slider-row">
-      <div className="slider reverse gap-2">
-        <img src={case2} />
-        <img src={case2Dois} />
-        <img src={case2Tres} />
-        <img src={case2} />
-      </div>
-    </div>
+        <div className="slider-row">
+            <div className="slider reverse gap-2">
+                <img src={case2} />
+                <img src={case2Dois} />
+                <img src={case2Tres} />
+                <img src={case2} />
+            </div>
+        </div>
   </div>
 
   <div className="a"></div>
@@ -54,7 +62,7 @@ return(
  
             </div>
 
-            <section className="mt-20">
+            <section className="mt-10">
 
                 <h1 className="text-2xl font-medium 
                 ">Tenha uma comunicação visual que <span className="font-bold">realmente converta.</span> </h1>
@@ -72,21 +80,70 @@ return(
                 </div>
 
             </section>
-
-            <section className="mt-20">
-
-                <h2 className="text-xl font-medium">Transformação de marca</h2>
-
-                <div className="linha"></div>
-
-                <div className="mt-10 flex flex-wrap gap-2 ">
-                    <img src={case1} className="w-40 rounded-4xl shadow-white/10 shadow-lg"></img>
-                    <img src={case1Dois} className="w-40 rounded-4xl shadow-white/10 shadow-lg"></img>
-                    <img src={case1Tres} className="w-full rounded-4xl shadow-white/10 shadow-lg"></img>
-                </div>
-
-            </section>
+            
         </div>
+
+            <div className="bg-white text-black px-5 rounded-t-3xl ">
+                <section className="mt-20">
+                    <h2 className="text-xl font-medium pt-5">Transformação de marca</h2>
+                    <div className="linhaBranco"></div>
+
+                    <Swiper 
+                        pagination={{ clickable: true }}
+                        modules={[Navigation, Pagination, Autoplay]}
+                        spaceBetween={20}
+                        slidesPerView={1}
+                        
+                        className="w-[400px] mt-10 h-[500px]"
+                        ><style>
+                        {`
+            
+                        .swiper-pagination-bullet {
+                            background-color: #000F0F !important; /* azul (Tailwind blue-500) */
+                            opacity: 1;
+                            }
+                            .swiper-pagination-bullet-active {
+                            background-color: #202424 !important; /* verde (Tailwind green-500) */
+                            }
+                        `}
+                    </style>
+                    
+                     
+                        <SwiperSlide> 
+                            <div className="flex gap-2 mb-2"> 
+                                
+                                <img src={case1} className="w-40 rounded-4xl shadow-black/20 shadow-lg h-45"></img> 
+                                <img src={case1Dois} className="w-40 rounded-4xl shadow-black/20 shadow-lg h-45"></img> 
+                            </div> 
+
+                            <img src={case1Tres} className="w-full rounded-4xl shadow-black/20 shadow-lg"></img> 
+                        </SwiperSlide> 
+
+                        <SwiperSlide>
+                            <div className="flex gap-2 mb-2"> 
+                                
+                                <img src={case2} className="w-40 rounded-4xl shadow-black/20 shadow-lg h-45"></img> 
+                                <img src={case2Dois} className="w-40 rounded-4xl shadow-black/20 shadow-lg h-45"></img> 
+                            </div> 
+
+                            <img src={case2Tres} className="w-full h-65 rounded-4xl shadow-black/20 shadow-lg"></img> 
+                        </SwiperSlide>
+                        
+                        
+                            
+                    </Swiper>
+
+                    <div className="flex justify-center items-center pb-10">
+                            <a className="bg-linear-to-r from-green-600 via-green-400 to-green-300 animate-gradient p-3 text-xl font-bold rounded-2xl mt-2 b-2 shadow-black/10 shadow-lg cursor-pointer hover:mt-[-4px] hover:mb-[5px] transition-all duration-500 "
+                            href="https://wa.me/62982616305" target="_blank"
+                            >
+                                COMEÇAR MEU PROJETO
+                
+                            </a>
+                        </div>
+                </section>
+            </div>
+        
 
     </body>
 
